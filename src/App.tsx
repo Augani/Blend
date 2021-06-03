b4281dee434023f1ca21465c0874db22fb2e3d17
Augustus Otu <Augani7@gmail.com>
2021-06-03 10:46:33 +0000

    added home and produxt pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+const Home = React.lazy(() => import("./pages/Home"));
+const Product = React.lazy(() => import("./pages/Product"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dark:bg-gray-800 w-screen h-screen">
+     <Suspense fallback={Loader}>
+     <Router>
+        <Route exact path="/p/:id" component={Product} />
+        <Route exact path="/" component={Home} />
+      </Router>
+     </Suspense>
+    </div>
+  );
+}
+
+function Loader(): JSX.Element {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="w-20 h-20 rounded-full border-blue-500 border-t animate-spin">
+        <div className="w-20 h-20 rounded-full border-green-500 border-b"></div>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+function index({}: Props): JSX.Element {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default index;
